15983f94a27022089068a59e4abc289ec446d2a6
Patryk Okpara <patrykokpara@hotmail.com>
2022-05-02 17:12:35 +0100

    timer test.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
 import {useAuth} from './contexts/AuthContext'
 import Header from './components/Header'
+import Timer from "./Timer";
+import Settings from "./Settings";
+import {useState} from "react";
+import SettingsContext from "./SettingsContext";
 
-export default function App() {
+function App() {
   const {isLoggedIn} = useAuth()
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
 
   return (
     <div className='App'>
       <Header />
-
+      <main>
+      <SettingsContext.Provider value={{
+        showSettings,
+        setShowSettings,
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+      </main>
       {isLoggedIn ? <LoggedInText /> : <LoggedOutText />}
     </div>
   )
@@ -22,3 +40,5 @@ const LoggedInText = () => {
 const LoggedOutText = () => (
   <p>Don't forget to start your backend server, then authenticate yourself.</p>
 )
+
+export default App;
