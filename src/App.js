2e29f0cd87e57e713765210debac8ec09840df27
Joe Thornton <thornton.jp@gmail.com>
2022-05-03 10:35:41 +0100

    Experimenting with react timer hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useTimer } from 'react-timer-hook';
 
-function App() {
+function MyTimer({ expiryTimestamp }) {
+  const {
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({ expiryTimestamp, autoStart: false, onExpire: () => console.warn('onExpire called') });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: 'center'}}>
+      <h1>react-timer-hook </h1>
+      <p>Pomodoro</p>
+      <div style={{fontSize: '100px'}}>
+        <span>{minutes}</span>:<span>{seconds}</span>
+      </div>
+      <button onClick={start}>Start</button>
+      <button onClick={() => {
+        // Restarts to 25 minutes timer
+        const time = new Date();
+        time.setSeconds(time.getSeconds() + 1500);
+        restart(time, false)
+      }}>Stop</button>
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + 1500); // 25 minutes timer
+  return (
+    <div>
+      <MyTimer expiryTimestamp={time} />
+    </div>
+  );
+}
