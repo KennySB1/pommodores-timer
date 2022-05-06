867d041f0f585c4b3ed60d810b6db33a3c1c44df
Joe Thornton <thornton.jp@gmail.com>
2022-05-06 12:22:59 +0100

    Added timer
    Co-authored-by: Patryk Okpara <Lorn888@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,34 @@
 import {useAuth} from './contexts/AuthContext'
 import Header from './components/Header'
 import { Suggestions } from './components/Suggestions'
+import Timer from "./components/Timer";
+import Settings from "./components/Settings";
+import {useState} from "react";
+import SettingsContext from "./components/SettingsContext";
+
 
 export default function App() {
   const {isLoggedIn} = useAuth()
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
 
   return (
     <div className='App'>
       <Header />
-
+      <main>
+      <SettingsContext.Provider value={{
+        showSettings,
+        setShowSettings,
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
       <Suggestions />
+      </main>
       {isLoggedIn ? <LoggedInText /> : <LoggedOutText />}
     </div>
   )
