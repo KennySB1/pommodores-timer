5fb3934d6b1d8672c1da2ff71b83a9c7ca653a99
Joe Thornton <thornton.jp@gmail.com>
2022-05-06 09:51:20 +0100

    removed redundant backend routes
    removed routes for serving react app from express server

--- server/index.js
+++ server/index.js
@@ -1,25 +1,15 @@
 require("dotenv").config(); // Secures variables
 const app = require("./utils/app"); // Backend App (server)
 const mongo = require("./utils/mongo"); // MongoDB (database)
-const express = require("express");
 const { PORT } = require("./constants");
 const authRoutes = require("./routes/auth");
 
 async function bootstrap() {
   await mongo.connect();
 
-
-  const path = require("path");
-
-  app.use(express.static(path.resolve(__dirname, "../client/build")));
-  app.get("/", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-  });
-
   // app.get('/', (req, res) => res.status(200).json({message: 'Hello World!'}))
   app.get("/healthz", (req, res) => res.status(200).send());
   app.use("/auth", authRoutes);
-
 
   app.listen(PORT, () => {
     console.log(`✅ Server is listening on port: ${PORT}`);
